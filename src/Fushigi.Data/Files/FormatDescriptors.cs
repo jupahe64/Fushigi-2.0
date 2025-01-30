dd6da45fe574567efa96f82c4d73cfdac0c01380
jupahe64 <jupahe64@users.noreply.github.com>
2025-01-30 04:08:23 +0100

    [Fushigi.Data] begin implementing Gyml types

---
+++ src/Fushigi.Data/Files/FormatDescriptors.cs
@@ -0,0 +1,14 @@
+﻿using BymlLibrary;
+using Fushigi.Data.BymlSerialization;
+
+namespace Fushigi.Data.Files;
+
+public static class FormatDescriptors
+{
+    public static FileFormatDescriptor<TGymlType> GetGymlFormat<TGymlType>()
+        where TGymlType : SerializableBymlObject<TGymlType>, new()
+        => new(IsCompressed: false, ReadGymlFile<TGymlType>);
+    private static TGymlType ReadGymlFile<TGymlType>(ArraySegment<byte> bytes)
+        where TGymlType : SerializableBymlObject<TGymlType>, new()
+        => SerializableBymlObject<TGymlType>.DeserializeFrom(Byml.FromBinary(bytes));
+}
---
+++ src/Fushigi.Data/Files/GymlTypes/WorldList.cs
@@ -0,0 +1,26 @@
+﻿using Fushigi.Data.BymlSerialization;
+
+namespace Fushigi.Data.Files.GymlTypes;
+
+/// <summary>
+/// Serializable game.stage.WorldList
+/// </summary>
+public class WorldList : SerializableBymlObject<WorldList>
+{
+    public static readonly string GymlTypeSuffix = "game__stage__WorldList";
+    public static readonly string[] DefaultSavePath = ["Stage", "WorldList"];
+    
+    public List<string> WorldMapStagePath = null!;
+    
+    #region De/Serialization
+    protected override void Deserialize(Deserializer d)
+    {
+        d.SetArray(ref WorldMapStagePath, "WorldMapStagePath", d.ConvertString);
+    }
+
+    protected override void Serialize(Serializer s)
+    {
+        s.SetArray(ref WorldMapStagePath, "WorldMapStagePath", s.ConvertString);
+    }
+    #endregion
+}
